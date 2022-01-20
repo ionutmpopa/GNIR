af88f4ac3a7dbf93a4c1835b67c1c9161ec055e7
ionutmpopa <Caragiale6>
2022-01-20 19:45:51 +0200

    Initialize project: first commit

---
+++ src/main/java/ro/iopo/gnir/GnirApplication.java
@@ -0,0 +1,13 @@
+package ro.iopo.gnir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnirApplication.class, args);
+    }
+
+}
