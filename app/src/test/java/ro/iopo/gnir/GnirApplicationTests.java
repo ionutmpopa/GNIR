a6cd66f0fe47cd7f9d22af5ff2ebc28f75980796
ionutmpopa <Caragiale6>
2022-01-21 20:36:23 +0200

    Multi-module project.

---
+++ app/src/test/java/ro/iopo/gnir/GnirApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.iopo.gnir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
